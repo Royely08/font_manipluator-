ffd384f5089151799bd5db39f9243db2d18200d6
Lakshna S <78756909+Royely08@users.noreply.github.com>
2021-09-06 20:48:29 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,42 @@
+noseX = 0;
+noseY = 0;
+rightWristX = 0;
+leftWristX = 0;
+difference = 0;
+
 function setup(){
     video = createCapture(VIDEO);
-    video.size(500,550);
+    video.size(550,500);
 
-    canvas = createCanvas(500,550);
+    canvas  = createCanvas(550,500);
     canvas.position(560,150);
 
     poseNet = ml5.poseNet(video,modelLoaded);
     poseNet.on('pose',gotPoses);
 }
 
-function draw{
-    background('#969A97')
+function draw(){
+    background('#969A97');
+    fill('#f90093');
+    stroke('#f90093');
+    square(noseX,noseY,difference);
 }
 
 function modelLoaded(){
-    console.log('PoseNet is Initialized')
+    console.log('poseNet is Initialized');
 }
 
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("Nose X = " + noseX + ", Nose Y = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+
+        difference = floor(leftWristX - rightWristX);
+        console.log("Left Wrist X = " + leftWristX + ", Right Wrist X = " + rightWristX + ", Difference = " +  difference);
     }
 }
